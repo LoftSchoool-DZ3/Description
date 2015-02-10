92aa8740b9c826d200d986e63fa8796b9b616940
lena_pavlova <isuzu69@gmail.com>
2015-02-11 01:01:38 +0400

    add concatCss, add images and its styles

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@
   var rename = require("gulp-rename");
   var uglify = require('gulp-uglify');
   var useref = require('gulp-useref');
+  var concatCss = require('gulp-concat-css');
 
   gulp.task('default', function(){
     console.log('Hello from Gulp!')
