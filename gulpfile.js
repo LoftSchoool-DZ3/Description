512532246c8dcc957cfdaef29f8136edb62d8ce4
lena_pavlova <isuzu69@gmail.com>
2015-02-11 16:42:44 +0400

    create frame

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,39 @@
-  var gulp = require('gulp');
-  var autoprefixer = require('gulp-autoprefixer');
-  var gulpif = require('gulp-if');
-  var rename = require("gulp-rename");
-  var uglify = require('gulp-uglify');
-  var useref = require('gulp-useref');
-  var concatCss = require('gulp-concat-css');
+var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+var gulpif = require('gulp-if');
+var rename = require("gulp-rename");
+var uglify = require('gulp-uglify');
+var useref = require('gulp-useref');
+var concatCss = require('gulp-concat-css');
 
-  gulp.task('default', function(){
-    console.log('Hello from Gulp!')
-  });
+gulp.task('default', ['ap','copy','useref']);
+
+
+gulp.task('ap', function() {
+	gulp.src('app/css/styles.css')
+	  	.pipe(autoprefixer({
+	        browsers: ['last 20 versions']
+	    }))
+	    .pipe(gulp.dest('app/css/'));
+});
+
+gulp.task('copy', function () {
+    gulp.src('app/img/*')
+        .pipe(gulp.dest('dist/img'));
+
+});
+
+gulp.task('useref', function () {
+    var assets = useref.assets();
+    
+    return gulp.src('app/index.html')
+	        .pipe(assets)
+	        .pipe(assets.restore())
+	        .pipe(useref())
+	        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch("app/*", ['default']);
+	gulp.watch("app/*/*", ['default']);
+});
