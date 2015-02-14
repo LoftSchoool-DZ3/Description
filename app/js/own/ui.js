61ee2b8484b35de943af2c5b067e735daa5c7c9f
Sergey Blashkin <serhii.blashkin@gmail.com>
2015-02-14 16:18:59 +0200

    Now slider is changing vatermark opacity

--- app/js/own/ui.js
+++ app/js/own/ui.js
@@ -1,8 +1,24 @@
-$(document).ready(function() {
-    $('.slider').slider({
-      orientation: "horizontal",
-      range: "min",
-      max: 100,
-      value: 30
+jQuery(document).ready(function($) {
+    "use strict";
+
+    var $slider = $('.slider'),
+        $vatermark = $('.v-watermark');
+
+    $slider.slider({
+        orientation: "horizontal",
+        range: "min",
+        max: 100,
+        min: 0,
+        value: 30,
+        slide: function(event, ui) {
+            console.log($slider.slider('value'));
+            var sliderValue = $slider.slider('value');
+            $vatermark.css('opacity', sliderValue / 100);
+        }
     });
+
+
+
+
+
   });
