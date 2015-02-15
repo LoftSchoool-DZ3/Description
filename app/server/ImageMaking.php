31c224a66820ce98529b567ef580460d72b7801f
dake <elessarelfstone@mail.ru>
2015-02-16 01:25:39 +0500

    111

--- app/server/ImageMaking.php
+++ app/server/ImageMaking.php
@@ -2,7 +2,21 @@
 	
 	require_once 'lib/WideImage.php';
 
-	define("FILES_PATH", "files");
+	define("FILES_PATH", "files/source");
+	define("RESULT_FILES_PATH", "files/result");
+
+
+
+	function generateString($length = 8){
+	  $chars = 'abdefhiknrstyzABDEFGHKNQRSTYZ23456789';
+	  $numChars = strlen($chars);
+	  $string = '';
+	  for ($i = 0; $i < $length; $i++) {
+	    $string .= substr($chars, rand(1, $numChars) - 1, 1);
+	  }
+	  return $string;
+	}
+
 
 
 	class ImageMaker{
@@ -41,21 +55,19 @@
 		{
 			$result = false;
 			if (in_array(exif_imagetype($this->watermarkImagePath), array(IMAGETYPE_GIF, IMAGETYPE_JPEG,  IMAGETYPE_PNG))){
-				$this->
+				$this->imgType = exif_imagetype($this->origImagePath);
 				$result = true;
-
-			}
-
-				
+			}			
 			return $result;
 		}
 
 		function MakeImage($left = 0, $top = 0, $opacity = 50){			
 			 $image = WideImage::load($this->origImagePath);
 			 $watermark = WideImage::load($this->watermarkImagePath);
-			 $new_image = $image->merge($watermark,$left, $top, $opacity);
-			 $new_image->saveToFile('files/test.gif');
-			 echo $this->watermarkImagePath;
+			 $new_image = $image->merge($watermark, $left, $top, $opacity);
+			 $newImagePath = 'files/result/'.generateString(5).image_type_to_extension($this->imgType);
+			 $new_image->saveToFile($newImagePath);			 
+			 return $newImagePath;
 		}
 }
 ?>
