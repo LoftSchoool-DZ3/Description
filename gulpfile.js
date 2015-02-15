3a2c779b8a6bd351b0239617d355781b0fa40e74
lena_pavlova <isuzu69@gmail.com>
2015-02-15 22:13:41 +0400

    correct gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,31 @@
+var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+var gulpif = require('gulp-if');
+var rename = require("gulp-rename");
+var uglify = require('gulp-uglify');
+var useref = require('gulp-useref');
+var concatCss = require('gulp-concat-css');
+var wiredep = require('wiredep').stream;
+
+//bower wiredep
+gulp.task('bower', function () {
+  gulp.src('./app/index.html')
+    .pipe(wiredep({
+      directory: 'app/bower_components'
+    }))
+    .pipe(gulp.dest('app'));
+});
+
+
+gulp.task('default', ['copy','ap','useref']);
+
+
+gulp.task('ap', function() {
+    gulp.src('app/css/styles.css')
+          .pipe(autoprefixer({
+            browsers: ['last 20 versions']
+        }))
+        .pipe(gulp.dest('app/css/'));
 var gulp = require('gulp'),
 connect = require('gulp-connect');
 
@@ -9,10 +37,28 @@ gulp.task('connect', function() {
   });
 });
 
+gulp.task('copy', function () {
+    gulp.src('app/img/*')
+        .pipe(gulp.dest('dist/img'));
 
-// gulp.task('default', ['copy','ap','useref']);
+    gulp.src('app/fonts/*')
+        .pipe(gulp.dest('dist/fonts'));
+
+});
+
+gulp.task('useref', function () {
+    var assets = useref.assets();
+    
+    return gulp.src('app/index.html')
+            .pipe(assets)
+            .pipe(assets.restore())
+            .pipe(useref())
+            .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('default', ['copy','ap','useref']);
 
 gulp.task('watch', function() {
-	gulp.watch("app/*", ['default']);
-	gulp.watch("app/*/*", ['default']);
-});
+    gulp.watch("app/*", ['default']);
+    gulp.watch("app/*/*", ['default']);
+}
