fcdc38984f972696716a5bbb6c5085abeaedcc6a
Sergey Ermakov <xom94ok@gmail.com>
2015-02-18 12:58:04 +0300

    Переделал file-load, добавил показ картинок области после загрузки

--- app/js/own/file-load.js
+++ app/js/own/file-load.js
@@ -1,30 +1,66 @@
 $(document).ready(function() {
-    var app = {
-          
-        initialize : function () {
+    var fileup = {
+
+        initialize: function() {
+            this.$inputFile = $('.fileupload')
+
+            
             this.setUpListeners();
+            this.fileUploading();
         },
-  
-        setUpListeners: function () {
-            $('#orig-img').change(function() {
-                $('#orig-img-name').html($(this).val().replace( "C:\\fakepath\\", '' ));
-                $('#orig-img').attr('value', $(this).val().replace( "C:\\fakepath\\", '' ));
-                if ($(this).val() == '') {
-                    $('#orig-img-name').html('Ошибка. Повторите еще раз.');
-                }
-            });
 
-            $('#watermark').change(function() {
-                $('#watermark-name').html($(this).val().replace( "C:\\fakepath\\", '' ));
-                $('#watermark').attr('value', $(this).val().replace( "C:\\fakepath\\", '' ));
-                if ($(this).val() == '') {
-                    $('#watermark-name').html('Ошибка. Повторите еще раз.');
-                }
+        setUpListeners: function() {
+            this.$inputFile.on('change', function() {
+                var changedInput = $('#' + this.id);
+                fileup.pasteFileName(changedInput);
+
             });
+        },
+
+        // Подстановка имени файла в инпут:
+        pasteFileName: function(fn) {
+            var inputFileId = $(fn),
+                fileNameSpan = $(inputFileId).prevAll('#' + inputFileId.attr('id') + '-name'),
+                inputFileVal = inputFileId.val().replace("C:\\fakepath\\", '');
+            
+            fileNameSpan.html(inputFileVal);
+            inputFileId.attr('value', inputFileVal);
+        },
+
+        //Загрузка файлов через плагин JQuery-File-Upload
+        fileUploading: function() {
+            'use strict';
+            // Change this to the location of your server-side upload handler:
+            var url =  'server/';
+            $('.fileupload').fileupload({
+                    url: url,
+                    dataType: 'json',
+                    disableImageResize: /Android(?!.*Chrome)|Opera/
+                        .test(window.navigator && navigator.userAgent),
+                    imageMaxWidth: 651,
+                    imageMaxHeight: 534,
+                    done: function() {
+                        var currentInputId = this.id;
+
+                        fileup.showFile(currentInputId);
+                    },
+                    progressall: function() {}
+
+                });
+
+        },
+
+        //Отображение загруженного файла в области просмотра
+        showFile: function(n){
+            var value = $('#' + n + '-name').text(),
+                viewImgClass = '.v-'+n ,
+                path = 'server\/files\/' + value ;
+            
+            $(viewImgClass).children().attr('src', path);
+
+
         }
-       
     }
-  
-    app.initialize();
-});
 
+    fileup.initialize();
+});
