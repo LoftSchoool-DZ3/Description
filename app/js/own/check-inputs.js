daa8bb0cc04e647f939ee422c5d87cc1f026e198
lena_pavlova <isuzu69@gmail.com>
2015-02-24 23:29:06 +0300

    validate too big values

--- app/js/own/check-inputs.js
+++ app/js/own/check-inputs.js
@@ -18,5 +18,26 @@ $(document).ready(function() {
 		if (this.value.match(/[^0-9]/g)) {
 			this.value = this.value.replace(/[^0-9]/g, '');
 		}
+
+		var origWidth = $('.layer.v-orig-img').width();
+		var watermarkWidth = $('.layer.v-watermark').width();
+		var maxWatermarkLeft = origWidth - watermarkWidth;
+		
+
+		var origHeight = $('.layer.v-orig-img').height();
+		var watermarkHeight = $('.layer.v-watermark').height();
+		var maxWatermarkTop = origHeight - watermarkHeight;
+	
+
+		if (this.value > maxWatermarkLeft && this.id == "x-one") {
+			this.value = maxWatermarkLeft;
+			$('.layer.v-watermark').css('left', maxWatermarkLeft);
+		}
+
+		if (this.value > maxWatermarkTop && this.id == "y-one") {
+			this.value = maxWatermarkTop;
+			$('.layer.v-watermark').css('top', maxWatermarkTop);
+		}
+		
 	})
 });
